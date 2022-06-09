befdc4eadb4c1d3933572404dc213c3ed7ea1dd3
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-09 16:52:21 +0300

    commit tree b9742c73cba54256920274537a0fc84a97aa7420

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+//1. titleCased
+//===============
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    const wordsInTutorial = tutorial.split(" "); //set up comma-separation
+    // console.log(wordsInTutorial);
+    const splitWords = wordsInTutorial.map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    });
+    // console.log(splitWords);
+    const capitalizedPhrase = splitWords.join(" "); //replace comma with space
+    // console.log(capitalizedPhrase);
+    return capitalizedPhrase;
+  });
+};
+
+// console.log(titleCased());
